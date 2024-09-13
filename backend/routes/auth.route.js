346aac7fc4bbf934a0165aea08516f27a126e1cf
inul74 <zmusthofa74@gmail.com>
2024-09-14 00:12:44 +0700

    protect route middleware

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  login,
+  logout,
+  signup,
+  getCurrentUser,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.get("/me", protectRoute, getCurrentUser);
 
 export default router;
