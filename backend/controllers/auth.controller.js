f26604adadb5363ba0a98afbe9ec50c2956864f7
inul74 <zmusthofa74@gmail.com>
2024-09-13 23:51:37 +0700

    welcome email, login & logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,8 @@
-import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+
+import User from "../models/user.model.js";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 
 export const signup = async (req, res) => {
   try {
@@ -12,53 +14,93 @@ export const signup = async (req, res) => {
 
     const existingEmail = await User.findOne({ email });
     if (existingEmail) {
-			return res.status(400).json({ message: "Email already exists" });
-		}
+      return res.status(400).json({ message: "Email already exists" });
+    }
 
     const existingUsername = await User.findOne({ username });
-		if (existingUsername) {
-			return res.status(400).json({ message: "Username already exists" });
-		}
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
 
     if (password.length < 6) {
-			return res.status(400).json({ message: "Password must be at least 6 characters" });
-		}
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
 
     const salt = await bcrypt.genSalt(10);
-		const hashedPassword = await bcrypt.hash(password, salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-		const user = new User({
-			name,
-			email,
-			password: hashedPassword,
-			username,
-		});
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      username,
+    });
 
-		await user.save();
+    await user.save();
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
 
-		res.cookie("jwt-inuldev", token, {
-			httpOnly: true, // prevent XSS attack
-			maxAge: 3 * 24 * 60 * 60 * 1000,
-			sameSite: "strict", // prevent CSRF attacks,
-			secure: process.env.NODE_ENV === "production", // prevents man-in-the-middle attacks
-		});
+    res.cookie("jwt-inuldev", token, {
+      httpOnly: true, // prevent XSS attack
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      sameSite: "strict", // prevent CSRF attacks,
+      secure: process.env.NODE_ENV === "production", // prevents man-in-the-middle attacks
+    });
 
-		res.status(201).json({ message: "User registered successfully" });
+    res.status(201).json({ message: "User registered successfully" });
 
-    // TODO: send welcome email
+    const profileUrl = process.env.CLIENT_URL + "/profile/" + user.username;
 
+    try {
+      await sendWelcomeEmail(user.email, user.name, profileUrl);
+    } catch (emailError) {
+      console.error("Error sending welcome Email", emailError);
+    }
   } catch (error) {
     console.log("Error in signup: ", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };
 
-export const login = (req, res) => {
-  res.send("login");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // Create and send token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+    await res.cookie("jwt-inuldev", token, {
+      httpOnly: true,
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.json({ message: "Logged in successfully" });
+  } catch (error) {
+    console.error("Error in login controller:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("logout");
+  res.clearCookie("jwt-inuldev");
+  res.json({ message: "Logged out successfully" });
 };
---
+++ backend/emails/emailHandlers.js
@@ -0,0 +1,80 @@
+import { mailtrapClient, sender } from "../lib/mailtrap.js";
+import {
+  createCommentNotificationEmailTemplate,
+  createConnectionAcceptedEmailTemplate,
+  createWelcomeEmailTemplate,
+} from "./emailTemplates.js";
+
+export const sendWelcomeEmail = async (email, name, profileUrl) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Welcome to Inuldev",
+      html: createWelcomeEmailTemplate(name, profileUrl),
+      category: "welcome",
+    });
+
+    console.log("Welcome email sent successfully", response);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const sendCommentNotificationEmail = async (
+  recipientEmail,
+  recipientName,
+  commenterName,
+  postUrl,
+  commentContent
+) => {
+  const recipient = [{ email: recipientEmail }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "New comment on your post",
+      html: createCommentNotificationEmailTemplate(
+        recipientName,
+        commenterName,
+        postUrl,
+        commentContent
+      ),
+      category: "comment_notification",
+    });
+
+    console.log("Comment notification email sent successfully", response);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const sendConnectionAcceptedEmail = async (
+  senderEmail,
+  senderName,
+  recipientName,
+  profileUrl
+) => {
+  const recipient = [{ email: senderEmail }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: `${recipientName} accepted your connection request`,
+      html: createConnectionAcceptedEmailTemplate(
+        senderName,
+        recipientName,
+        profileUrl
+      ),
+      category: "connection_accepted",
+    });
+
+    console.log("Connection accepted email sent successfully", response);
+  } catch (error) {
+    throw error;
+  }
+};
